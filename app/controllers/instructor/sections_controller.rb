69f88e11718bb0e7e6a8d53d580d4f069018d048
Art Lobrin <artelo.studio@gmail.com>
2020-07-24 23:17:03 +0000

    trying heroku bug fix

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
-<<<<<<< HEAD
-=======
+# < HEAD
+# =======
   def new
     @course = Course.find(params[:course_id])
     @section = Section.new
@@ -16,5 +16,5 @@ class Instructor::SectionsController < ApplicationController
   def section_params
     params.require(:section).permit(:title)
   end
->>>>>>> ad98e7a0316f14abad8a2cfcbca8a799a1e2cba0
+ # ad98e7a0316f14abad8a2cfcbca8a799a1e2cba0
 end
