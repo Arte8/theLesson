d3c9d387dc481124691a0afbbf7a8a075ef121e2
Art <artelo.studio@gmail.com>
2020-08-11 03:03:46 +0000

    gen model ran migration and associations

--- db/migrate/20200811025732_create_lessons.rb
+++ db/migrate/20200811025732_create_lessons.rb
@@ -1,8 +1,14 @@
 class CreateLessons < ActiveRecord::Migration[5.2]
   def change
     create_table :lessons do |t|
+      t.string :title
+      t.string :subtitle
+      t.integer :section_id
+      t.timestamps
+    end
 
       t.timestamps
+      add_index :lessons, :section_id
     end
   end
 end
