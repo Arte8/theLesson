e54b50adee40e854fddef24a621ea56b240345b2
Art <artelo.studio@gmail.com>
2020-08-13 01:58:44 +0000

    permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
 before_action :authenticate_user!
+before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -18,6 +19,12 @@ before_action :authenticate_user!
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   helper_method :current_course
   def current_course
